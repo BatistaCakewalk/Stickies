39bf3ca25dc0dfb275f42019a44c5f87c6597540
Batista <118333712+BatistaCakewalk@users.noreply.github.com>
2026-08-10 22:20:04 -0400

    Brand-new Logging system.

---
+++ src/main/java/com/redtops/stickies/core/Logs/LogLevel.java
@@ -0,0 +1,17 @@
+package com.redtops.stickies.core.Logs;
+
+public enum LogLevel {
+    INFO("INFO"),
+    WARN("WARN"),
+    CRITICAL("CRITICAL"),
+    FATAL("FATAL"),
+    DEBUG("DEBUG");
+
+    private final String label;
+
+    LogLevel(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() { return label; }
+}
---
+++ src/main/java/com/redtops/stickies/core/Logs/TimeLog.java
@@ -0,0 +1,21 @@
+package com.redtops.stickies.core.Logs;
+
+// From geeksforgeeks. Because I cannot be bothered making my own, huge thanks to them
+// Source: https://www.geeksforgeeks.org/java/java-time-localdatetime-class-in-java/
+
+// Java Program to illustrate LocalDateTime Class by
+// Formatting LocalDateTime to string
+
+// Importing all classes from java.time package
+import java.time.LocalDateTime;
+import java.time.format.*;
+import java.util.*;
+
+// Main class
+class TimeLog {
+
+    public static String getTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a");
+        return LocalDateTime.now().format(formatter);
+    }
+}
