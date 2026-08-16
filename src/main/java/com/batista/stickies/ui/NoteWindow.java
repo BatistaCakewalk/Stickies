ff725e4bfa9300e0da3c878c7839f9a319d3277b
Batista <118333712+BatistaCakewalk@users.noreply.github.com>
2026-08-16 18:10:38 -0400

    Made handleNoteState and discardNoteState. It DOES get called but it's not writing to the new table.

--- src/main/java/com/batista/stickies/ui/NoteWindow.java
+++ src/main/java/com/batista/stickies/ui/NoteWindow.java
@@ -13,6 +13,7 @@ package com.batista.stickies.ui;
 import com.batista.stickies.core.Note;
 import com.batista.stickies.core.NoteManager;
 import com.batista.stickies.core.Logs.LogService;
+import com.batista.stickies.storage.StorageHandler;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -39,14 +40,18 @@ public class NoteWindow extends JFrame {
         LogService.info("NoteWindow constructor called | noteId=" + note.getId());
         this.note = note;
         this.noteManager = noteManager;
-        initWindow();
+        try {
+            initWindow();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         initComponents();
         makeDraggable();
         makeSizeable();
         LogService.info("NoteWindow fully initialized | noteId=" + note.getId());
     }
 
-    private void initWindow() throws IOException {
+    private void initWindow() throws IOException, SQLException {
         LogService.info("initWindow called | noteId=" + note.getId());
         setSize(note.getWidth(), note.getHeight());
         setLocation(note.getCordX(), note.getCordY());
@@ -69,6 +74,11 @@ public class NoteWindow extends JFrame {
         JButton closeButton = new JButton("X");
         closeButton.addActionListener(e -> {
             LogService.info("closeButton clicked | noteId=" + note.getId() + " | disposing window.");
+            try {
+                StorageHandler.discardNoteState(this.note.getId());
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
             dispose();
         });
         closeButton.setFocusable(false);
@@ -100,6 +110,11 @@ public class NoteWindow extends JFrame {
 
         getContentPane().setBackground(Color.decode(note.getColor()));
         LogService.info("initWindow complete | noteId=" + note.getId());
+        StorageHandler.handleNoteState(this.note.getId());
+    }
+
+    private void initRestoredWindows() throws IOException {
+
     }
 
     private void initComponents() {
